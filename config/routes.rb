ff112906e16151be1a3a62bbfb08b27d09f851d6
GrantRoholt <groholt@novillus.com>
2018-11-25 11:40:44 -0800

    Finish weight app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :weights
+  resources :users
   root to: 'visitors#index'
 end
---
+++ spec/factories/weights.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :weight do
+    weightvalue { 1.5 }
+    weightdate { "2018-11-25" }
+    user_id { 1 }
+  end
+end
